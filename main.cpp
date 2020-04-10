dfe5976431a9c91e98a30e8eefe0ce0eaba3ee78
SmetaninAS <smetaninas@mpei.com>
2020-04-10 13:58:23 +0300

    часть 1 и 2

--- main.cpp
+++ main.cpp
@@ -49,6 +49,7 @@ vector <size_t>  make_histogram(const vector<double> &numbers,size_t bin_count)
     }
 void show_histogram_text(const vector<size_t> &bins)
     {
+
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -82,8 +83,55 @@ void show_histogram_text(const vector<size_t> &bins)
     }
     }
 
+
+  void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+  void svg_end()
+    {
+    cout << "</svg>\n";
+
+    }
+void svg_text(double left, double baseline, string text)
+{
+cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
+
+}
+void svg_rect(double x, double y, double width, double height)
+{
+    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='red'" <<" fill='#ffeeee'/>";
+}
+void show_histogram_svg(const vector<size_t>& bins)
+{
+const auto IMAGE_WIDTH = 400;
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+const auto BLOCK_WIDTH = 10;
+double top = 0;
+  svg_begin(400, 300);
+for (size_t bin : bins)
+    {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
 int main() {
     //
+
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -92,13 +140,12 @@ int main() {
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-
     // Обработка данных
     double min , max;
     // Поиск максимума и минимума с помобщью процедуры
     find_minmax(numbers,min,max);
     const auto bins = make_histogram(numbers, bin_count);
     // Вывод данных
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
