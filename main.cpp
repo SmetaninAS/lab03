adc6e78c142f9466ba64c407444857c89ef73442
SmetaninAS <smetaninas@mpei.com>
2020-04-10 14:11:51 +0300

    Часть 2

--- main.cpp
+++ main.cpp
@@ -104,9 +104,9 @@ void svg_text(double left, double baseline, string text)
 cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
 
 }
-void svg_rect(double x, double y, double width, double height)
+void svg_rect(double x, double y, double width, double height,string stroke = "black" ,string fill = "black")
 {
-    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='red'" <<" fill='#ffeeee'/>";
+    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
 }
 void show_histogram_svg(const vector<size_t>& bins)
 {
@@ -123,7 +123,7 @@ for (size_t bin : bins)
     {
     const double bin_width = BLOCK_WIDTH * bin;
     svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"blue","#aaffaa");
     top += BIN_HEIGHT;
     }
     svg_end();
