6ae37241ee18a15935557d3311d9507ecb37ee11
smetanin <smetanin@mail.ru>
2020-05-08 15:12:11 +0300

    histogram

--- histogram.h
+++ histogram.h
@@ -2,6 +2,10 @@
 #define HISTOGRAM_H_INCLUDED
 #include <vector>
 using namespace std;
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 void find_minmax(const vector<double> &numbers, double& min, double& max);
 
 #endif // HISTOGRAM_H_INCLUDED
