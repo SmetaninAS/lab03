921e6bfbad132d5fab785aa810771817727caea8
smetanin <smetanin@mail.ru>
2020-05-08 10:51:16 +0300

    read_input

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include "histogram.h"
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in,size_t count)
 {
     vector<double> result(count);
@@ -13,7 +18,22 @@ vector<double> input_numbers(istream& in,size_t count)
     return result;
 }
 
+Input read_input(istream& in)
+{
+    Input data;
 
+    cerr << "Enter number count: ";
+    size_t number_count;
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> data.bin_count;
+
+    return data;
+}
 
 vector <size_t>  make_histogram(const vector<double> &numbers,size_t bin_count)
     {
@@ -116,21 +136,8 @@ for (size_t bin : bins)
 }
 
 int main() {
-    //
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-// Ввод числе заменен вызовом функции:
-   const auto numbers = input_numbers(cin,number_count);
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-    // Обработка данных
-    double min , max;
-    // Поиск максимума и минимума с помобщью процедуры
-    find_minmax(numbers,min,max);
-    const auto bins = make_histogram(numbers, bin_count);
-    // Вывод данных
+    const auto input= read_input(cin);
+    const auto bins = make_histogram(input.numbers,input.bin_count);
     show_histogram_svg(bins);
     return 0;
 }
