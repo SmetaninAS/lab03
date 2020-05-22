3a228304d934337985c58756cab370af27f49571
smetanin <smetanin@mail.ru>
2020-05-22 20:12:31 +0300

    4.2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <iomanip>
 #include "histogram.h"
+#include "svg.h"
 #define INFO_BUFFER_SIZE 32767
 using namespace std;
 
@@ -114,22 +115,12 @@ make_info_text() {
     DWORD version = info & mask;
     DWORD platform = info>>16;
     DWORD mask_major = 0b00000000'00000000'00000000'11111111;
-
-   // printf("%u", version_major);
-   // cerr<< ".";
-   //printf("%u\n", version_minor );
-
-    // TODO: получить версию системы, записать в буфер.
-    // TODO: получить имя компьютера, записать в буфер.
-
     char system_dir[MAX_PATH];
     GetSystemDirectory(system_dir, MAX_PATH);
-   // printf("System directory: %s", system_dir);
     cerr <<endl;
     TCHAR  infoBuf[INFO_BUFFER_SIZE];
     DWORD  bufCharCount = INFO_BUFFER_SIZE;
     GetComputerName( infoBuf, &bufCharCount );
-   //  printf("System name: %s", infoBuf);
      cerr<<endl;
      if ((info&0x80000000)==0)
      {
@@ -140,80 +131,6 @@ make_info_text() {
      buffer <<"System name : "<< infoBuf <<"  "<<"directory : "<< system_dir<<"  " <<"version : "<<version_major<<"."<<version_minor;
       }
     return buffer.str();
-}
-
-
-  void svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-  void svg_end()
-    {
-    cout << "</svg>\n";
-
-    }
-void svg_text(double left, double baseline, string text)
-{
-cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
-
-}
-void svg_rect(double x, double y, double width, double height,string stroke = "black" ,string fill = "black")
-{
-    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
-
-}
-void svg_line(double x1,double y1,double x2,double y2,string stroke_1,struct Input bindata)
-{
-    cout << "<line x1='"<<x1<<"' y1='"<<y1<<"' x2='"<<x2<<"' y2='"<<y2<<"' stroke='"<<stroke_1<<"' stroke-dasharray='"<<bindata.length_ch<<" "<<bindata.length_pr<<"' />";
-}
-void show_histogram_svg(const vector<size_t>& bins,struct Input bindata)
-{
-const auto IMAGE_WIDTH = 600;
-const auto IMAGE_HEIGHT = 300;
-const auto TEXT_LEFT = 20;
-const auto TEXT_BASELINE = 20;
-const auto TEXT_WIDTH = 50;
-const auto BIN_HEIGHT = 30;
-const auto BLOCK_WIDTH = 10;
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-double top = 0;
-string name = make_info_text();
-
-  svg_begin(600, 300);
-  size_t max_count = 0;
-         for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-     for (size_t bin : bins)
-        {
-            size_t height = bin;
-            if (scaling_needed)
-            {
-                const double scaling_factor = (double)MAX_ASTERISK / max_count;
-                height = (size_t)(bin * scaling_factor);
-            }
-            const double bin_width = BLOCK_WIDTH * height;
-            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-            svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"blue","#aaffaa");
-            if (top !=(bindata.bin_count-1)*BIN_HEIGHT)
-                svg_line(BLOCK_WIDTH,top+BIN_HEIGHT,IMAGE_WIDTH,top+BIN_HEIGHT,"red",bindata);
-            top += BIN_HEIGHT;
-        }
-    svg_text(TEXT_LEFT, top + TEXT_BASELINE, name);
-
-    svg_end();
 }
 
 size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
@@ -241,6 +158,9 @@ Input download(const string& address)
   if(res!=0)
     {
     cout<<curl_easy_strerror(curl_easy_perform(curl));
+    double ulspeed_curl=0;
+    if (CURLE_OK != curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE,&ulspeed_curl))
+        cerr <<curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE,&ulspeed_curl);
     exit(1);
     }
     curl_easy_cleanup(curl);
@@ -257,8 +177,9 @@ int main(int argc, char* argv[])
     } else {
         input = read_input(cin, true);
     }
+    string name = make_info_text();
     const auto bins = make_histogram(input);
-    show_histogram_svg(bins,input);
+    show_histogram_svg(bins,input,name);
     return 0;
 }
 
